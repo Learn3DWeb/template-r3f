86035b38fe8ea19dbfcec7752d0afbc19ea01f67
Brett Williams <brett@brettwilliams.net>
2024-04-09 16:34:32 -0500

    Update Leva title bar and add additional lights to Stage component.

--- src/components/3D/elements/Stage.jsx
+++ src/components/3D/elements/Stage.jsx
@@ -2,17 +2,23 @@ import { CameraControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import React from "react";
 import styled from "styled-components";
-import { AmbientLightLeva, DirectionalLightLeva, PointLightLeva, RectAreaLightLeva, SpotLightLeva } from "./lights";
+import {
+  AmbientLightLeva,
+  DirectionalLightLeva,
+  PointLightLeva,
+  RectAreaLightLeva,
+  SpotLightLeva,
+} from "./lights";
 
 export function Stage({ children, ...props }) {
   return (
     <CanvasStyle shadows>
-      <AmbientLightLeva />
+      {/* <AmbientLightLeva /> */}
       <DirectionalLightLeva />
-      <PointLightLeva />
+      {/* <PointLightLeva />
       <RectAreaLightLeva />
       <SpotLightLeva />
- 
+  */}
       {children}
       <CameraControls />
       <gridHelper args={[30, 30, 30]} />
--- src/components/3D/elements/lights/elements/DirectionalLightLeva.jsx
+++ src/components/3D/elements/lights/elements/DirectionalLightLeva.jsx
@@ -1,5 +1,7 @@
-import { folder, useControls } from "leva";
-import { useRef, useMemo, memo } from "react";
+import { memo, useMemo, useRef, useEffect } from "react";
+import { useControls, folder } from "leva";
+import { DirectionalLightHelper, CameraHelper } from "three";
+import { useHelper } from "@react-three/drei";
 
 // Function to generate a random 2-digit hexadecimal string
 function generateID(length = 3) {
@@ -14,7 +16,13 @@ export const DirectionalLightLeva = memo(
     name,
     color = "#ffffff",
     intensity = 1,
-    castShadow = false,
+    shadowBias = -0.005,
+    shadowRadius = 1,
+    shadowFar = 9,
+    cameraSize = 3,
+    castShadow = false, // Default is not to cast shadows
+    showHelper = false, // Default is not to show the helper
+    position = [0, 1, 1], // Default position is (0, 1, 1)
     ...props
   }) => {
     // Generate a unique ID for each DirectionalLight or use provided name
@@ -28,12 +36,21 @@ export const DirectionalLightLeva = memo(
         // Create a folder for DirectionalLight parameters
         [`directional:${ID}`]: folder(
           {
+            // Toggle for helper visibility
+            showHelper: { value: showHelper },
             // Hexadecimal color of the light
             color: { value: color },
             // Numeric value of the light's intensity
             intensity: { value: intensity, min: 0, step: 0.1 },
             // Toggle to cast shadows
             castShadow: { value: castShadow },
+            // Position of the light
+            position: { value: position },
+            // Additional shadow variables
+            shadowBias: { value: shadowBias, label: "Bias" },
+            shadowRadius: { value: shadowRadius, label: "Radius" },
+            shadowFar: { value: shadowFar, label: "Far" },
+            cameraSize: { value: cameraSize, label: "Size" },
           },
           { collapsed: true }
         ), // Collapse the folder by default
@@ -42,7 +59,33 @@ export const DirectionalLightLeva = memo(
     );
 
     // Create ref for DirectionalLight
+    // const directionalLightRef = useRef();
+    // const dCameraRef = useRef();
+
+    //useHelper(directionalLightVars.showHelper && dCameraRef, CameraHelper, 1, "hotpink");
+    // Create ref for point light
     const directionalLightRef = useRef();
+    const dCamera = useRef();
+
+    // Attach PointLightHelper to point light ref if showHelper is true
+    // useHelper(
+    //   directionalLightVars.showHelper ? directionalLightRef : { current: null },
+    //   DirectionalLightHelper,
+    //   0.5,
+    //   directionalLightVars.color
+    // );
+
+    // useEffect(() => {
+    //   dCamera.current = directionalLightRef.current ? directionalLightRef.current.shadow.camera : null;
+    // }, [directionalLightRef]);
+    useEffect(() => {
+      dCamera.current = directionalLightRef.current ? directionalLightRef.current.shadow.camera : null;
+    }, [directionalLightRef]);
+
+     useHelper(
+       directionalLightVars.showHelper ? dCamera : { current: null },
+       CameraHelper
+     );
 
     return (
       <>
@@ -50,6 +93,16 @@ export const DirectionalLightLeva = memo(
         <directionalLight
           {...{ ...directionalLightVars, ...props }}
           ref={directionalLightRef}
+          castShadow
+          shadow-mapSize-height={1024}
+          shadow-mapSize-width={1024}
+          shadow-camera-far={shadowFar}
+          shadow-camera-left={-cameraSize}
+          shadow-camera-right={cameraSize}
+          shadow-camera-top={cameraSize}
+          shadow-camera-bottom={-cameraSize}
+          shadow-bias={shadowBias}
+          shadow-radius={shadowRadius}
         />
       </>
     );
